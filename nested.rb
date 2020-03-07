73a28d37ab5b5b6966705ed5f329e955fd45b4e0
Lisa Jung <lisahjung@gmail.com>
2020-03-07 05:28:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,6 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:alan_kay][:languages]="Assembly"
+programmer_hash[:alan_kay][:languages][1]="Assembly"
 programmer_hash
 end
